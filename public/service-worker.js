a0ad8b3c76170cd804976d76c3089b25f798021f
luam1 <143610911+luam1@users.noreply.github.com>
2025-06-29 20:01:24 -0500

    Atualizar o service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,13 +1,37 @@
-self.addEventListener('install', (event) => {
-  console.log('Service Worker instalado');
-  self.skipWaiting();
-});
+const CACHE_NAME = 'cyber-glasshouse-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/app.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
 
-self.addEventListener('activate', (event) => {
-  console.log('Service Worker ativado');
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (event) => {
-  // Intercepta requisições - não altera respostas ainda
-  console.log('Requisição interceptada:', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
